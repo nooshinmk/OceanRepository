41515cf278e66df85943693e89cdc350b5f33f78
nooshinmk <nooshinmk@LAPTOP-1GR92659>
2018-06-09 12:19:05 +0200

    Add project files.

---
+++ OceanSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OceanSystem.Startup))]
+namespace OceanSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
